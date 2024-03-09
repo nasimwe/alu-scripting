c6673800845a9f45748324c763fbe4eae0d6d731
nasimwe <n.asimwe@alustudent.com>
2024-03-09 13:09:09 +0000

    fix 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-""" This module returns the number of subscribers """
+"""
+A script that use reddit api to outputs the
+number of subscribers of a certain subreddit.
+"""
+
+
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    BASE_URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Didas Junior'}
-    response = requests.get(
-        BASE_URL, headers=headers)
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    return 0
+        data = json.loads(response.text)
+        return data['data']['subscribers']
+
+    else:
+        return 0
